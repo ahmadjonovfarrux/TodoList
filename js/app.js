23913d65254fadaf61da80c0999a1e4851b2d6f5
Farrux <Farrux@gmail.com>
2025-11-16 00:23:36 +0500

    feat: enhance showTodos function to display todo items with edit and delete icons

--- js/app.js
+++ js/app.js
@@ -18,24 +18,63 @@ let todos = JSON.parse(localStorage.getItem("list"))
   ? JSON.parse(localStorage.getItem("list"))
   : [];
 
-  
+if (todos.length) {
+  showTodos();
+}
 
+// setTodos to localStorage
+function setTodos() {
+  localStorage.setItem("list", JSON.stringify(todos));
+}
 
-//  <li class="list-group-item d-flex justify-content-between">
-//           Hello world
-//           <div class="todos-icons">
-//             <span class="opacity-50 me-2">14.10.2025</span>
-//             <img
-//               src="./images/edit.svg"
-//               alt="edit icon"
-//               width="25"
-//               height="25"
-//             />
-//             <img
-//               src="./images/delete.svg"
-//               alt="delete icon"
-//               width="25"
-//               height="25"
-//             />
-//           </div>
-//         </li>
+// showTodos from localStorage to brauzer
+function showTodos() {
+  const todos = JSON.parse(localStorage.getItem("list"));
+  listGroupTodo.innerHTML = " ";
+  todos.forEach((item, i) => {
+    listGroupTodo.innerHTML += `
+     <li class="list-group-item d-flex justify-content-between">
+         ${item.text}
+          <div class="todos-icons">
+            <span class="opacity-50 me-2">14.10.2025</span>
+            <img
+              src="./images/edit.svg"
+              alt="edit icon"
+              width="25"
+              height="25"
+            />
+            <img
+              src="./images/delete.svg"
+              alt="delete icon"
+              width="25"
+              height="25"
+            />
+          </div>
+        </li>
+    `;
+  });
+}
+
+// showMessage function
+function showMessage(where, message) {
+  document.getElementById(`${where}`).textContent = `${message}`;
+
+  setTimeout(() => {
+    document.getElementById(`${where}`).textContent = " ";
+  }, 3000);
+}
+
+// get Todos
+formCreate.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoText = formCreate["input-create"].value.trim();
+  formCreate.reset();
+  if (todoText.length) {
+    todos.push({ text: todoText, time: "3:56, 14.11.2025", complated: false });
+    setTodos();
+    showTodos();
+    console.log(todos);
+  } else {
+    showMessage("message-create", "Please enter some text");
+  }
+});
