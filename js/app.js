49351316f49a8a6c3847b45f1d2453891dee0f7b
Farrux <Farrux@gmail.com>
2025-11-20 13:56:52 +0500

    feat: implement getTime function and update showTodos to display dynamic time

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const time = document.getElementById("time");
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
 const closeIcon = document.getElementById("close");
+// const deleteTodo = document.querySelector(".deleteTodo");
 // const messageCreate = document.getElementById("message-create");
 
 // time elements
@@ -27,6 +28,47 @@ function setTodos() {
   localStorage.setItem("list", JSON.stringify(todos));
 }
 
+// time function
+function getTime() {
+  const now = new Date();
+  const date = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+  const month =
+    now.getMonth() < 10 ? "0" + (now.getMonth() + 1) : now.getMonth() + 1;
+  const year = now.getFullYear();
+
+  const hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+  const minute =
+    now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+  const second =
+    now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "November",
+    "December",
+  ];
+
+  const monthTitle = now.getMonth();
+
+  fullDay.textContent = `${date} ${months[monthTitle]}, ${year}`;
+  hourEl.textContent = hour;
+  minuteEl.textContent = minute;
+  secondEl.textContent = second;
+  return ` ${hour}:${minute}, ${date}.${month}.${year}`;
+}
+
+setInterval(() => {
+  getTime();
+}, 1000);
+
 // showTodos from localStorage to brauzer
 function showTodos() {
   const todos = JSON.parse(localStorage.getItem("list"));
@@ -36,7 +78,7 @@ function showTodos() {
      <li class="list-group-item d-flex justify-content-between">
          ${item.text}
           <div class="todos-icons">
-            <span class="opacity-50 me-2">14.10.2025</span>
+            <span class="opacity-50 me-2">${item.time}</span>
             <img
               src="./images/edit.svg"
               alt="edit icon"
@@ -48,6 +90,7 @@ function showTodos() {
               alt="delete icon"
               width="25"
               height="25"
+              onClick=(deleteTodo(${i}))
             />
           </div>
         </li>
@@ -70,11 +113,20 @@ formCreate.addEventListener("submit", (e) => {
   const todoText = formCreate["input-create"].value.trim();
   formCreate.reset();
   if (todoText.length) {
-    todos.push({ text: todoText, time: "3:56, 14.11.2025", complated: false });
+    todos.push({ text: todoText, time: getTime(), complated: false });
     setTodos();
     showTodos();
-    console.log(todos);
+    // console.log(todos);
   } else {
     showMessage("message-create", "Please enter some text");
   }
 });
+
+// delete TodoList function
+function deleteTodo(id) {
+  const deletedTodos = todos.filter((item, i) => {
+    return i !== id;
+  });
+
+  console.log(deletedTodos);
+}
